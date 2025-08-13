3ea13cf57bc2e7d73031428af61145defaf66929
squarfie <59784389+squarfie@users.noreply.github.com>
2025-08-13 09:50:10 +0800

    added timezone

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+TIME_ZONE = "Asia/Manila"
